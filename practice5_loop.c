5a338a293c91ec9ed0da0895f76b5ab4fd6ca367
HosenJahangir <hosen.jahangir29@gmail.com>
2023-07-14 03:52:46 +0300

    checking

---
+++ practice5_loop.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+
+{
+    int N;
+    scanf("%d", &N);
+    if (N <= 1000)
+    {
+        for (int i = 1; i <= N; i++)
+        {
+            if (i % 5 == 0)
+            {
+                printf("%d Yes\n", i);
+            }
+            else
+            {
+                printf("%d No\n", i);
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ problem2_summation.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int A;
+    int B;
+    scanf("%d %d", &A, &B);
+    int sum = A + B;
+    // summation
+    printf("%d", sum);
+
+    return 0;
+}
